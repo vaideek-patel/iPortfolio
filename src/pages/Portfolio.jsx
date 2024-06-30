3bbbcdc7f02f3c7b332d1e9e00dcd6f66d7a2c71
Vaideek Patel <84132883+vaideekpatel@users.noreply.github.com>
2024-06-30 11:21:16 +0530

    [FEAT] : Added Modal Popup in Portfolio Section on Zoom in Icon Press

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -21,6 +21,9 @@ import sal from 'sal.js';
 const Portfolio = () => {
     const navigate = useNavigate();
     const [activeLink, setActiveLink] = useState('ALL');
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [selectedIndex, setSelectedIndex] = useState(0); // Track the index of the selected image
 
     const links = ['ALL', 'APP', 'PRODUCT', 'BRANDING', 'BOOKS'];
 
@@ -41,12 +44,30 @@ const Portfolio = () => {
 
     const filteredData = activeLink === 'ALL' ? data : data.filter(item => item.category.toUpperCase() === activeLink);
 
-    const handleZoomInClick = () => {
-        console.log(`Zoom in clicked`);
+    const handleZoomInClick = (item, index) => {
+        setSelectedImage(item);
+        setSelectedIndex(index); // Set the index of the selected image
+        setIsModalOpen(true);
     };
 
     const handleLinkClick = () => {
         navigate("/portfolio_details");
+    };
+
+    const handleNext = () => {
+        const currentCategoryItems = data.filter(item => item.category === selectedImage.category);
+        const currentIndex = currentCategoryItems.findIndex(item => item.id === selectedImage.id);
+        const nextIndex = (currentIndex + 1) % currentCategoryItems.length;
+        setSelectedImage(currentCategoryItems[nextIndex]);
+        setSelectedIndex(nextIndex);
+    };
+
+    const handlePrev = () => {
+        const currentCategoryItems = data.filter(item => item.category === selectedImage.category);
+        const currentIndex = currentCategoryItems.findIndex(item => item.id === selectedImage.id);
+        const prevIndex = (currentIndex - 1 + currentCategoryItems.length) % currentCategoryItems.length;
+        setSelectedImage(currentCategoryItems[prevIndex]);
+        setSelectedIndex(prevIndex);
     };
 
     useEffect(() => {
@@ -56,7 +77,7 @@ const Portfolio = () => {
     useEffect(() => {
         sal({ once: true });
     }, [filteredData]);
-    
+
     return (
         <div className="mb-8 p-5 bg-sky-50">
             <SectionTitle title={"Portfolio"} />
@@ -75,7 +96,7 @@ const Portfolio = () => {
                     ))}
                 </nav>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                    {filteredData.map(item => (
+                    {filteredData.map((item, index) => (
                         <div key={item.id} className="relative" data-sal="slide-up" data-sal-duration="1000">
                             <div className="border bg-black shadow overflow-hidden group">
                                 <div className='overflow-hidden'>
@@ -86,21 +107,47 @@ const Portfolio = () => {
                                     <p className="text-xs font-semibold">{item.name}</p>
                                 </div>
                                 <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                                    <button className="" onClick={() => handleZoomInClick(item.name)}>
+                                    <button className="" onClick={() => handleZoomInClick(item, index)}>
                                         <img src={zoomInIcon} alt="Zoom In" className="w-6 h-6" />
                                     </button>
                                     <button className="p-5" onClick={() => handleLinkClick(item.name)}>
                                         <img src={linkIcon} alt="Link" className="w-6 h-6" />
                                     </button>
                                     <div className="absolute bottom-4 p-2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                                    <p className="text-md font-semibold">{item.simpleText}</p>
-                                </div>
+                                        <p className="text-md font-semibold">{item.simpleText}</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     ))}
                 </div>
             </div>
+            {isModalOpen && selectedImage && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+                    <button
+                        className="absolute top-2 right-4 text-gray-400 text-5xl  rounded-full"
+                        onClick={() => setIsModalOpen(false)}
+                    >
+                        &times;
+                    </button>
+                    <button
+                        className="absolute left-0 text-white text-3xl ml-5 p-2 rounded-full"
+                        onClick={handlePrev}
+                    >
+                        <i className="bi bi-arrow-left"></i>
+                    </button>
+                    <button
+                        className="absolute right-0 text-white text-3xl mr-5 p-2 rounded-full"
+                        onClick={handleNext}
+                    >
+                        <i className="bi bi-arrow-right"></i>
+                    </button>
+                    <div className="bg-white max-w-screen-lg flex flex-col items-center relative">
+                        <img src={selectedImage.image} alt={selectedImage.name} className="max-w-[100%] max-h-[calc(100vh-85px)] mx-auto transition-transform duration-300" />
+                        <p className="text-lg text-left w-full p-6 py-7 bg-white">{selectedImage.name}</p>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
